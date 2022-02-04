56c375a092ab2e4a9b1fd56178104a08a019a51f
Péter Dvorák <83779344+RPRKLR@users.noreply.github.com>
2022-02-04 13:48:01 +0100

    Add files via upload

---
+++ toolbar/toolbar.cpp
@@ -0,0 +1,22 @@
+#include "toolbar.h"
+
+Toolbar::Toolbar(const wxString& title)
+       : wxFrame(NULL, wxID_ANY, title, 
+                 wxDefaultPosition, wxSize(300, 250)) {
+
+    wxImage::AddHandler(new wxPNGHandler);
+
+    wxBitmap exit(wxT("exit.png"), wxBITMAP_TYPE_PNG);
+
+    wxToolBar *toolbar = CreateToolBar();
+    toolbar->AddTool(wxID_EXIT, wxT("Exit application"), exit);
+    toolbar->Realize();
+
+    Connect(wxID_EXIT, wxEVT_COMMAND_TOOL_CLICKED, 
+        wxCommandEventHandler(Toolbar::onQuit));
+}
+
+void Toolbar::onQuit(wxCommandEvent& WXUNUSED(event)) {
+    
+    Close(true);
+}
